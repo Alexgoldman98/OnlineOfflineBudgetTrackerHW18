7e3a69fbe7f8ecedc8e3661f1379984ca2cf943f
Alexandra <Alexgoldman98@gmail.com>
2021-03-17 15:17:03 -0400

    adding screenshots to readme

--- public/db.js
+++ public/db.js
@@ -1,9 +1,7 @@
 let db;
-// create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
-   // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
@@ -11,7 +9,6 @@ request.onupgradeneeded = function(event) {
 request.onsuccess = function(event) {
   db = event.target.result;
 
-  // check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
@@ -22,22 +19,16 @@ request.onerror = function(event) {
 };
 
 function saveRecord(record) {
-  // create a transaction on the pending db with readwrite access
   const transaction = db.transaction(["pending"], "readwrite");
 
-  // access your pending object store
   const store = transaction.objectStore("pending");
 
-  // add record to your store with add method.
   store.add(record);
 }
 
 function checkDatabase() {
-  // open a transaction on your pending db
   const transaction = db.transaction(["pending"], "readwrite");
-  // access your pending object store
   const store = transaction.objectStore("pending");
-  // get all records from store and set to a variable
   const getAll = store.getAll();
 
   getAll.onsuccess = function() {
@@ -52,18 +43,14 @@ function checkDatabase() {
       })
       .then(response => response.json())
       .then(() => {
-        // if successful, open a transaction on your pending db
         const transaction = db.transaction(["pending"], "readwrite");
 
-        // access your pending object store
         const store = transaction.objectStore("pending");
 
-        // clear all items in your store
         store.clear();
       });
     }
   };
 }
 
-// listen for app coming back online
 window.addEventListener("online", checkDatabase);
